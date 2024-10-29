540637281e80c06654ebe6edebfc3d6012174dfd
OlekZaremba <sarutern.gra@gmail.com>
2024-10-29 13:18:24 +0100

    Naprawa struktury repozytorium - dodanie backendu i frontendu bez podrepozytoriów

---
+++ Backend/Filmer-Backend/src/main/java/com/filmer/filmerbackend/FilmerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.filmer.filmerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmerBackendApplication.class, args);
+    }
+
+}
