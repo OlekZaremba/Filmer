cb55b19f522d1e10779162fd65987b2baaffe937
OlekZaremba <sarutern.gra@gmail.com>
2024-11-27 12:26:09 +0100

    Fixed bug with the repository and optimized the query

--- Backend/Filmer-Backend/src/main/java/com/filmer/filmerbackend/Repositories/UsersRepository.java
+++ Backend/Filmer-Backend/src/main/java/com/filmer/filmerbackend/Repositories/UsersRepository.java
@@ -6,13 +6,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UsersRepository extends JpaRepository<Users, Integer> {
     Optional<Users> findByNick(String nick);
-    @Query("SELECT u FROM users u WHERE u.id_user IN (SELECT f.user2 FROM FriendsList f WHERE f.user1 = :userId)")
+    @Query("SELECT u FROM users u JOIN friends_list f ON u.id_user = f.user2 WHERE f.user1 = :userId")
     List<Users> findFriendsByUserId(@Param("userId") int userId);
+
 
 }
