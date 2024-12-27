476e22d8ff81d7ff3de28a02582ddc79d17b1dce
jakubbonczyk <jakubbonczyk@icloud.com>
2024-12-27 13:00:42 +0100

    Added popup to library

--- Frontend/Filmer-Frontend/src/app/components/library/library.component.ts
+++ Frontend/Filmer-Frontend/src/app/components/library/library.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {NgForOf, NgIf} from '@angular/common';
 import {ReactiveFormsModule} from '@angular/forms';
+import { MoviePopupComponent } from '../movie-popup/movie-popup.component';
 
 @Component({
   selector: 'app-library',
@@ -8,11 +9,23 @@ import {ReactiveFormsModule} from '@angular/forms';
   imports: [
     NgForOf,
     NgIf,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MoviePopupComponent
   ],
   templateUrl: './library.component.html',
   styleUrl: './library.component.css'
 })
-export class LibraryComponent {
+export class LibraryComponent {  isPopupVisible = false;
+  selectedMovieTitle = 'Tytuł filmu';
+  selectedMovieDescription = 'Opis filmu.';
 
+  showMovieInfo(title: string, description: string): void {
+    this.selectedMovieTitle = title;
+    this.selectedMovieDescription = description;
+    this.isPopupVisible = true;
+  }
+
+  closePopup(): void {
+    this.isPopupVisible = false;
+  }
 }
