fdd290c492c4eac56c182db8f5687bd44cbd660f
jakubbonczyk <jakubbonczyk@icloud.com>
2024-12-28 23:55:18 +0100

    Frontent library changes

--- Frontend/Filmer-Frontend/src/app/components/movie-popup/movie-popup.component.ts
+++ Frontend/Filmer-Frontend/src/app/components/movie-popup/movie-popup.component.ts
@@ -1,27 +1,38 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { NgIf } from '@angular/common';
 import {PdfService} from '../../services/pdf.service';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-movie-popup',
   standalone: true,
-  imports: [NgIf],
+  imports: [NgIf, FormsModule],
   template: `
-    <div
-      class="popup-overlay"
-      *ngIf="isVisible"
-      (click)="closePopup()"
-    >
-      <div
-        class="popup-content"
-        (click)="$event.stopPropagation()"
-      >
+    <div class="popup-overlay" *ngIf="isVisible" (click)="closePopup()">
+      <div class="popup-content" (click)="$event.stopPropagation()">
         <h2>{{ title }}</h2>
         <p>{{ description }}</p>
+        <h2>Recenzja</h2>
+        <input list="review" placeholder="Twoja recenzja" onfocus="this.value=''">
+
         <button class="download-button" (click)="downloadPdf()">Pobierz opis do PDF</button>
         <button class="close-button" (click)="closePopup()">Zamknij</button>
       </div>
     </div>
+
+    <datalist id="review">
+        <option value="Brak opinii">
+        <option value="1">
+        <option value="2">
+        <option value="3">
+        <option value="4">
+        <option value="5">
+        <option value="6">
+        <option value="7">
+        <option value="8">
+        <option value="9">
+        <option value="10">
+    </datalist>
   `,
   styleUrls: ['./movie-popup.component.css'],
 })
